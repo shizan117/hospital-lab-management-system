bb04e192eb24e12afadf57e7b3e16eebf1835ba0
shizan <shizan117@gmail.com>
2025-06-06 11:44:48 +0600

    footer updated

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -12,7 +12,7 @@
                     <a href="#" class="social-icon"><i class="fab fa-instagram"></i></a>
                 </div>
             </div>
-            <div class="col-lg-2 col-md-6">
+            <div class="col-lg-2 col-md-6 col-6">
                 <h5 class="text-white mb-4">Quick Links</h5>
                 <ul class="list-unstyled">
                     <li class="mb-2"><a href="{{route('home')}}" class="text-white-50 text-decoration-none">Home</a></li>
@@ -22,7 +22,7 @@
                     <li class="mb-2"><a href="{{route('contact')}}" class="text-white-50 text-decoration-none">Contact</a></li>
                 </ul>
             </div>
-            <div class="col-lg-3 col-md-6">
+            <div class="col-lg-3 col-md-6 col-6">
                 <h5 class="text-white mb-4">Our Services</h5>
                 <ul class="list-unstyled">
                     <li class="mb-2"><a href="{{route('services')}}" class="text-white-50 text-decoration-none">Pathology Tests</a></li>
@@ -35,7 +35,7 @@
                 <h5 class="text-white mb-4">Contact Info</h5>
                 <ul class="list-unstyled text-white-50">
                     <li class="mb-2"><i class="fas fa-map-marker-alt me-2"></i> Main Dhaka - Khulna High Way, Rajbari 7710</li>
-                    <li class="mb-2"><i class="fas fa-phone-alt me-2"></i>  01316-****</li>
+                    <li class="mb-2"><i class="fas fa-phone-alt me-2"></i> 01316-****</li>
                     <li class="mb-2"><i class="fas fa-envelope me-2"></i> info@medicaregoalanda.com</li>
                 </ul>
             </div>
@@ -43,12 +43,32 @@
         <hr class="mt-4 mb-3 bg-white-10">
         <div class="row">
             <div class="col-md-6 text-center text-md-start">
-                <p class="mb-0 text-white-50">&copy; 2025 Medicare Diagnostic Lab Goalanda. All rights reserved.</p>
+                <p class="mb-0 text-white-50">© 2025 Medicare Diagnostic Lab Goalanda. All rights reserved.</p>
             </div>
             <div class="col-md-6 text-center text-md-end">
-
                 <a href="https://savageoff.com/" target="#">SavageOff</a>
             </div>
         </div>
     </div>
+
+    <style>
+        @media (max-width: 767.98px) {
+            .col-6 {
+                flex: 0 0 50%;
+                max-width: 50%;
+                padding-left: 8px;
+                padding-right: 8px;
+            }
+            .col-6 h5 {
+                font-size: 1rem;
+                margin-bottom: 1.5rem;
+            }
+            .col-6 ul li {
+                font-size: 0.9rem;
+            }
+            .col-6 ul li a {
+                font-size: 0.9rem;
+            }
+        }
+    </style>
 </footer>
